e15d3cd505bc73c478b99a8ebcf3329bb69befbb
dceleste35 <145990752+dceleste35@users.noreply.github.com>
2024-11-27 15:03:07 +0100

    Refactor show-candidate.blade.php to remove unnecessary tabindex attribute and keydown event handler

--- resources/views/livewire/show-candidate.blade.php
+++ resources/views/livewire/show-candidate.blade.php
@@ -105,10 +105,8 @@
                     :ref="`${tab}Tab`"
                     :aria-selected="activeTab === tab"
                     :aria-controls="`${tab}-panel`"
-                    :tabindex="activeTab === tab ? 0 : -1"
                     :class="activeTab === tab ? 'border-b-2 border-purple-500 text-purple-100 bg-purple-500' : 'text-gray-500 hover:text-gray-700'"
                     @click="activeTab = tab"
-                    @keydown="handleKeyNav($event, tab)"
                     x-text="tab.charAt(0).toUpperCase() + tab.slice(1)"
                 ></button>
             </template>
